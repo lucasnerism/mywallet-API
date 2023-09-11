d54543bc1b33a868d1ecffea6ea167c37acc018c
Lucas Neris <lucasnerism@gmail.com>
2023-09-10 23:09:30 -0300

    FIX: adjusting tests setup

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -6,7 +6,6 @@ dotenv.config();
 export const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
   await mongoClient.connect();
-  console.log("Database Connected");
 } catch (err) {
   console.log(err);
 }
--- tests/integration/transactions.e2e-spec.js
+++ tests/integration/transactions.e2e-spec.js
@@ -41,13 +41,46 @@ describe('Transactions e2e Tests', () => {
       const { status } = await api.get('/transactions');
       expect(status).toEqual(401);
     });
-    it('sould return user transactions', async () => {
+    it('should return user transactions', async () => {
       const user = await createUser({ name: 'test', email: 'test@test.com', password: '123456' });
       const token = await logInUser(user.insertedId);
       const transaction = await createTransaction({ type: 'in', description: 'test', value: 10, userId: user.insertedId });
       const { status, body } = await api.get('/transactions').set('Authorization', `Bearer ${token}`);
       expect(status).toEqual(200);
       expect(JSON.stringify(body)).toEqual(JSON.stringify([transaction]));
+    });
+  });
+
+  describe('PUT /transactions/:id', () => {
+    it('should return status 401 with invalid token', async () => {
+      const { status } = await api.put('/transactions');
+      expect(status).toEqual(401);
+    });
+    it('should update user transaction', async () => {
+      const user = await createUser({ name: 'test', email: 'test@test.com', password: '123456' });
+      const token = await logInUser(user.insertedId);
+      const transaction = await createTransaction({ type: 'in', description: 'test', value: 10, userId: user.insertedId });
+      const newTransaction = { type: 'in', description: 'test', value: 20 };
+      const { status } = await api.put(`/transactions/${transaction._id}`).set('Authorization', `Bearer ${token}`).send(newTransaction);
+      expect(status).toEqual(200);
+      const transactions = await db.collection('transactions').find().toArray();
+      expect(JSON.stringify(transactions)).toEqual(JSON.stringify([{ ...transaction, value: 20 }]));
+    });
+  });
+
+  describe('DELETE /transactions/:id', () => {
+    it('should return status 401 with invalid token', async () => {
+      const { status } = await api.put('/transactions');
+      expect(status).toEqual(401);
+    });
+    it('should delete user transaction', async () => {
+      const user = await createUser({ name: 'test', email: 'test@test.com', password: '123456' });
+      const token = await logInUser(user.insertedId);
+      const transaction = await createTransaction({ type: 'in', description: 'test', value: 10, userId: user.insertedId });
+      const { status } = await api.delete(`/transactions/${transaction._id}`).set('Authorization', `Bearer ${token}`);
+      expect(status).toEqual(200);
+      const transactions = await db.collection('transactions').find().toArray();
+      expect(transactions).toHaveLength(0);
     });
   });
 });
